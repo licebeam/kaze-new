730b67884ee70572d68fe1b8cc24856aa09e089a
Dustin Walker <dustinwalkerart@gmail.com>
2018-06-24 17:19:21 +0900

    card update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,26 @@ import styled from 'styled-components';
 
 const CardBody = styled.div`
   width: 100%;
-  background-color: #000;
   color: #fff;
   flex: 1;
   justify-content: center; 
   display: flex; 
+  flex-direction: column; 
 
   .card-center { 
+    flex: 1;
     margin-top: 20px;
     color: #000;
     border: 2px solid transparent; 
     border-radius: 30px;
-    width: 90%;
-    height: 200px;
+    width: 100%;
+    height: 300px;
     text-align: center; 
-    background-color: #eee;
+    background-color: #fff;
+  }
+  .button-container { 
+    flex: .6;
+    background-color: #fff;
   }
 `
 
@@ -28,6 +33,9 @@ const Card = ({
   return (
     <CardBody>
       <div className='card-center'>This is a flashcard</div>
+      <div className='button-container'>
+        <button></button>
+      </div> 
     </CardBody>
   )
 }
