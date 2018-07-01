312b289b2b2d08f9121e47e69c2bf0bfb4403ddc
Dustin Walker <dustinwalkerart@gmail.com>
2018-07-02 01:00:57 +0900

    router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import KazeContainer from './KazeContainer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 
 ReactDOM.render(<KazeContainer />, document.getElementById('root'));
 
