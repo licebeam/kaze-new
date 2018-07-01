6260f0cb6f2b3a40601305d8d7361b5a2f0c1a31
Dustin Walker <dustinwalkerart@gmail.com>
2018-07-02 02:30:26 +0900

    added firebase ui and adjusted the firebase auth into index.js

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -3,6 +3,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import KazeBody from './components/KazeBody';
 import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 const Container = styled.div `
   margin: 0 auto; 
@@ -11,13 +16,26 @@ const Container = styled.div `
   background-color: #eee;
   height: 100vh;
 `
+
+const dbAdd = () => {
+
+}
 class LogIn extends Component {
   render() {
     return (
       <Container>
-        <h1>Welcome to My Awesome App</h1>
+        <h1>Kaze App test</h1>
         <div id="firebaseui-auth-container"></div>
         <div id="loader">Loading...</div>
+        <div>
+          <button onClick={() => {
+            dbAdd();
+          }}>
+            <Link to='/Home'>
+              Continue without logging in?
+            </Link>
+          </button>
+        </div>
       </Container>
     );
   }
