de4a14f29d7d12e4c3d0c05fa26be4116bfcf9c9
Dustin Walker <dustinwalkerart@gmail.com>
2018-07-15 22:18:26 +0900

    fixed api

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -17,7 +17,12 @@ import UserProfile from './userprofile/UserProfile'
 import AdminPanel from './adminPanel';
 
 var config = {
-
+  apiKey: "AIzaSyC9kgL1DXJ-rjmVq7J6ghqofptEFajpb_0",
+  authDomain: "kazeapp.firebaseapp.com",
+  databaseURL: "https://kazeapp.firebaseio.com",
+  projectId: "kazeapp",
+  storageBucket: "kazeapp.appspot.com",
+  messagingSenderId: "92317655588",
 };
 
 firebase.initializeApp(config);
